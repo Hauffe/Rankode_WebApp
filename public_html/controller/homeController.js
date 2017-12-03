9d014186e354f0581f07159293b02e983ff3e39d
Alexandre <Alexandre@Alexandre-PC>
2017-12-03 16:01:03 -0200

    login 1.2 - sessionController

--- public_html/controller/homeController.js
+++ public_html/controller/homeController.js
@@ -5,18 +5,6 @@
 
 $(document).ready(function(){
     
-    var RANKODE_URL = "http://localhost:41115/service/api/";
-    var GIT_URL = "";
-    var url = "http://localhost:8383/RankodeFrontEnd/";
-    var login = sessionStorage.getItem('login');
-    
-        $("#userName").append(
-                    "<img src='images/img.png' alt=''>"+ sessionStorage.getItem('firstName')+" "+ sessionStorage.getItem('lastName') +" "+
-                    "<span class='fa fa-angle-down'></span>");
-    
-	$("#logout").click(function(){
-            sessionStorage.clear();
-            window.location.replace(url+'/login.html');
-	});
+
 });
 
